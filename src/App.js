921af374a509a4daeb56e4fd2abad91abf0cd820
neowonbin <wonbin.jang@gmail.com>
2019-04-28 08:36:22 +0900

    5.1 Customer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Customer from './component/Customer'
 import './App.css';
 
 const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
   'name':'Donald Trump',
   'birthday':'20190427',
   'gender':'Male',
@@ -13,6 +15,8 @@ class App extends Component {
   render() {
     return(
       <Customer
+        id={customer.id}
+        image={customer.image}
         name={customer.name}
         birthday={customer.birthday}
         gender={customer.gender}
