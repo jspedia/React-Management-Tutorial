b680475f8b3bc56066a4999d871fcad6dd533035
neowonbin <wonbin.jang@gmail.com>
2019-04-28 08:44:59 +0900

    5.2 Customer

--- src/App.js
+++ src/App.js
@@ -2,27 +2,63 @@ import React, { Component } from 'react';
 import Customer from './component/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+  {
   'id': 1,
   'image': 'https://placeimg.com/64/64/1',
   'name':'Donald Trump',
   'birthday':'20190427',
   'gender':'Male',
   'job':'Employee'
-}
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name':'Mac Donald',
+    'birthday':'20190428',
+    'gender':'Male',
+    'job':'Casher'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name':'Burger King',
+    'birthday':'20190426',
+    'gender':'Male',
+    'job':'Server'
+  }      
+]
 
 class App extends Component {
   render() {
     return(
-      <Customer
-        id={customer.id}
-        image={customer.image}
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
-    );
+      <div>
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        />
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        />      
+      </div>
+      );
   }
 }
 
