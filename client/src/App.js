86c3a6f0a524cd7ae68c6b39d17eb56523eb6323
neowonbin <wonbin.jang@gmail.com>
2019-04-29 00:07:53 +0900

    8.1 Proxy Error

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const styles = theme => ({
   }
 })
 
-
+/*
 const customers = [
   {
   'id': 1,
@@ -47,8 +47,26 @@ const customers = [
     'job':'Server'
   }      
 ]
+*/
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props; 
     return(
@@ -65,21 +83,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {
-              customers.map(c => {
-                return (
-                  <Customer
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                );    
-              })
-            }
+            { this.state.customers ? this.state.customers.map(c => {
+                  return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );    
+                }) : ""}              
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name':'Donald Trump',
+        'birthday':'20190427',
+        'gender':'Male',
+        'job':'Employee'
+        },
+        {
+            'id': 2,
+            'image': 'https://placeimg.com/64/64/2',
+            'name':'Mac Donald',
+            'birthday':'20190428',
+            'gender':'Male',
+            'job':'Casher'
+        },
+        {
+            'id': 3,
+            'image': 'https://placeimg.com/64/64/3',
+            'name':'Burger King',
+            'birthday':'20190426',
+            'gender':'Male',
+            'job':'Server'
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); 
